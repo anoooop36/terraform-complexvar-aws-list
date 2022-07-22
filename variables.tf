e74d42925e9e238568f8d5cffc2ee109d7bd6a08
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-07-22 21:37:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,16 @@
-variable "sampleTags" {
-  default = ["First Volume Tag", "Second Volume Tag", "Third Volume Tag"]
-  type = list
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+ type    = string
+ default = "us-east-1"
 }
 
-variable "ebsSizes" {
-  type = list
-  default = [40, 80, 120]
+variable "sampleObject" {
+  type    = object({size=number, tag=string, isDev=bool})
+  default = {
+    size  : 40
+    tag   = "Dev ebs volume", 
+    isDev = true
+  }
 }
